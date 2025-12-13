2e648172a9f1c2b0fea31cc8287bddf899997d12
aetta <chiape@gmail.com>
2025-12-13 20:31:16 -0300

    .

--- quick-email-capture.php
+++ quick-email-capture.php
@@ -1,20 +1,24 @@
 <?php
 /*
 Plugin Name: Quick Email Capture
+Plugin URI: https://github.com/aetta/quick-email-capture
 Description: Simple, fast and lightweight email capture. No bloat.
 Version: 1.0.1
 Requires at least: 6.0
 Requires PHP: 7.4
 Author: aetta
+Author URI: https://github.com/aetta
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: quick-email-capture
 Domain Path: /languages
 */
 
-if (!defined('ABSPATH')) exit;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-define('QEC_VERSION', '2.0.1');
+define('QEC_VERSION', '1.0.1');
 define('QEC_PLUGIN_FILE', __FILE__);
 define('QEC_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('QEC_PLUGIN_URL', plugin_dir_url(__FILE__));
